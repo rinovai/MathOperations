5b46cfdc0a0bc107e8ed63d7466c6a3e155514de
rinovai <pikokk@bk.ru>
2022-08-22 18:07:53 +0600

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 40
+        int b = 30
+        System.out.println(sum(a, b));
+    }
+
+    public static int sum(int value1, int value2) {
+
+        int result = value1 + value2;
+        return result;
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 40;
+        int b = 30;
+        int c = 10;
+        int d = 20;
+        System.out.println(sum(a, b, c, d));
+    }
+
+    public static int sum(int value1, int value2, int value3, int value4) {
+
+        int result = value1 + value2 + value3 + value4;
+        return result;
+
+
+    }
+}
